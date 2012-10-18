e39a3461d744e7675c571d4ae226a6c162cc4d9e
DevMike <zarechenskiy.mihail@gmail.com>
2012-10-18 15:29:44 +0300

    show nested locations in the admin backend

--- app/admin/countries.rb
+++ app/admin/countries.rb
@@ -9,6 +9,20 @@ ActiveAdmin.register Country do
 
   filter :russian_name
 
+  show do
+    panel 'Attributes' do
+      attributes_table_for resource do
+        rows :id, :name, :russian_name, :created_at, :updated_at
+      end
+    end
+
+    panel 'Regions' do
+      table_for(resource.regions) do |t|
+        t.column(:russian_name){|region| link_to region.russian_name, admin_region_path(region) }
+      end
+    end
+  end
+
   form do |f|
     f.inputs "Details" do
       f.input :name
--- app/admin/regions.rb
+++ app/admin/regions.rb
@@ -10,6 +10,20 @@ ActiveAdmin.register Region do
   filter :russian_name
   filter :country, :as => :select, :collection => Country.order(:name).map(&:name)
 
+  show do
+    panel 'Attributes' do
+      attributes_table_for resource do
+        rows :id, :name, :russian_name, :created_at, :updated_at
+      end
+    end
+
+    panel 'Settlements' do
+      table_for(resource.settlements) do |t|
+        t.column(:russian_name){|settlement| link_to settlement.russian_name, admin_settlement_path(settlement) }
+      end
+    end
+  end
+
   form do |f|
     f.inputs "Details" do
       f.input :name
